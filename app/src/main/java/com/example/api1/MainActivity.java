9b8592502602883e8e624b1ee3bda900cb7dd458
Tawhidur Rahman Rafid <55914303+Rafid-tawhid@users.noreply.github.com>
2021-09-01 03:01:59 +0600

    multiple object tring to show in an array but an error

--- app/src/main/java/com/example/api1/MainActivity.java
+++ app/src/main/java/com/example/api1/MainActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.api1.part3.ArrayDesignClass;
+import com.example.api1.part3.ObjectDesignClass;
+import com.example.api1.part3.PositionClass;
+import com.example.api1.part3.TeamPlayer;
+
 import java.util.List;
 
 import retrofit2.Call;
@@ -15,6 +20,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    OurRetrofitClient ourRetrofitClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +30,11 @@ public class MainActivity extends AppCompatActivity {
                 .baseUrl("https://cricket.sportmonks.com/api/v2.0/")
                 .addConverterFactory(GsonConverterFactory.create()).build();
 
-        OurRetrofitClient ourRetrofitClient=retrofit.create(OurRetrofitClient.class);
+        ourRetrofitClient=retrofit.create(OurRetrofitClient.class);
 
 //       Call<OurMainDataClass> call= ourRetrofitClient.getData("BvI7ZMd5UxW8Xl1thH6pbKJdbXWSUBJxEbSuze8PjxgA9y0wvHEkPv26Pqz1");
-
-
+//
+//
 //        call.enqueue(new Callback<OurMainDataClass>() {
 //            @Override
 //            public void onResponse(Call<OurMainDataClass> call, Response<OurMainDataClass> response) {
@@ -36,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
 //                if (response.isSuccessful())
 //                {
 //
+//                    Log.d("aa",response.toString());
 //                    List<ObjectDataClass> list=response.body().getData();
 //
 //                    for (ObjectDataClass objectDataClass:list)
@@ -60,31 +67,85 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
         //call by id
-        Call<CallById> call= ourRetrofitClient.getData(5,"BvI7ZMd5UxW8Xl1thH6pbKJdbXWSUBJxEbSuze8PjxgA9y0wvHEkPv26Pqz1");
 
-        call.enqueue(new Callback<CallById>() {
+
+
+//        call.enqueue(new Callback<CallById>() {
+//            @Override
+//            public void onResponse(Call<CallById> call, Response<CallById> response) {
+//                if (response.isSuccessful()) {
+//
+//                    ObjectDataClass objectDataClass = response.body().getData();
+//
+//                    Log.d("aa",objectDataClass.getName());
+//                    Log.d("aa",objectDataClass.getResource());
+//                    Log.d("aa",String.valueOf(objectDataClass.getId()));
+//                    Log.d("aa",objectDataClass.getUpdated_at());
+//
+//
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<CallById> call, Throwable t) {
+//
+//            }
+//        });
+
+        Call<TeamPlayer> call= ourRetrofitClient.getData(10,"BvI7ZMd5UxW8Xl1thH6pbKJdbXWSUBJxEbSuze8PjxgA9y0wvHEkPv26Pqz1");
+
+
+        call.enqueue(new Callback<TeamPlayer>() {
             @Override
-            public void onResponse(Call<CallById> call, Response<CallById> response) {
-                if (response.isSuccessful()) {
+            public void onResponse(Call<TeamPlayer> call, Response<TeamPlayer> response) {
 
-                    ObjectDataClass objectDataClass = response.body().getData();
+                Log.d("bb",String.valueOf(response.isSuccessful()));
 
-                    Log.d("aa",objectDataClass.getName());
-                    Log.d("aa",objectDataClass.getResource());
-                    Log.d("aa",String.valueOf(objectDataClass.getId()));
-                    Log.d("aa",objectDataClass.getUpdated_at());
+                Log.d("bb",String.valueOf(response.body().toString()));
+                int cid=190324;
 
+                String name=response.body().getName();
+                Log.d("bb",String.valueOf(response.body().getCountry_id()));
 
-                }
+                Call<ArrayDesignClass> playerCall= ourRetrofitClient.getPlayerData("BvI7ZMd5UxW8Xl1thH6pbKJdbXWSUBJxEbSuze8PjxgA9y0wvHEkPv26Pqz1",cid);
 
+                playerCall.enqueue(new Callback<ArrayDesignClass>() {
+                    @Override
+                    public void onResponse(Call<ArrayDesignClass> call, Response<ArrayDesignClass> response) {
+
+                        Log.d("bbb",String.valueOf(response.isSuccessful()));
+
+                        List<ObjectDesignClass> list=response.body().getData();
+
+                        for (ObjectDesignClass obj:list)
+                        {
+                            String dob=obj.getDateofbirth();
+                            String fullname=obj.getFullname();
+                            String gender=obj.getGender();
+                            PositionClass positionClass=obj.getPositionClass();
+                            String name=positionClass.getName();
+                            Log.d("aaa",String.valueOf(cid));
+                            Log.d("aaa",String.valueOf(dob));
+                            Log.d("aaa",String.valueOf(fullname));
+                            Log.d("aaa",String.valueOf(gender));
+                            Log.d("aaa",String.valueOf(name));
+
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ArrayDesignClass> call, Throwable t) {
+
+                    }
+                });
             }
 
             @Override
-            public void onFailure(Call<CallById> call, Throwable t) {
+            public void onFailure(Call<TeamPlayer> call, Throwable t) {
 
             }
         });
-
 
     }
 }
--- app/src/main/java/com/example/api1/OurRetrofitClient.java
+++ app/src/main/java/com/example/api1/OurRetrofitClient.java
@@ -1,5 +1,10 @@
 package com.example.api1;
 
+import android.util.Log;
+
+import com.example.api1.part3.ArrayDesignClass;
+import com.example.api1.part3.TeamPlayer;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -9,7 +14,14 @@ public interface OurRetrofitClient {
 
 //    @GET("continents")
 //    Call<OurMainDataClass> getData(@Query("api_token") String token);
- @GET("continents/{id}")
- Call<CallById> getData(@Path ("id") int id,@Query("api_token") String token);
+// @GET("continents/{id}")
+// Call<CallById> getData(@Path ("id") int id,@Query("api_token") String token);
+   @GET("teams/{id}")
+   Call<TeamPlayer> getData(@Path ("id") int id, @Query("api_token") String token);
+
+   @GET("players")
+    Call<ArrayDesignClass> getPlayerData(@Query("api_token") String token
+     , @Query("country_id") int cid
+    );
 
 }
